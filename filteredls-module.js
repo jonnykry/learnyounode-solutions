7e94442942635c1cda7af5326492e9be216cabba
jonnykry <jonnykry93@gmail.com>
2014-07-29 19:52:13 -0500

    Module solution.

--- filteredls-module.js
+++ filteredls-module.js
@@ -1,12 +1,7 @@
-require('fs');
-require('./lslib.js');
+var fs = require('fs');
+var lslib = require('./lslib.js');
 
 var path = process.argv[2];
 var filter = process.argv[3];
 
-lslib(path, filter, function(err, list) {
-	for (i = 0; i < list.length; i++) {
-		console.log(list[i]);
-	}
-});
-
+lslib(path, filter, function(err, list) {});
--- lslib.js
+++ lslib.js
@@ -1,17 +1,19 @@
-require('fs');
+var fs = require('fs');
+
+var arr = [];
 
 module.exports = function(path, filter, callback) {
 	fs.readdir(path, function(err, list) {
-        if (err)
-		return callback(err);
+	    if (err) {
+				return callback(err);
+			}
 
-	for (i = 0; i < list.length; i++) {
-                if (list[i].indexOf('.' + filter) > -1) {
-                        console.log(list[i]);
-                }
-        }
-
-	callback(null, list);
-});
-
-
+			for (i = 0; i < list.length; i++) {
+		    if (list[i].indexOf('.' + filter) > -1) {
+		      console.log(list[i]);
+					arr.push(list[i])
+		    }
+			}
+			callback(null, arr);
+		}
+)};
